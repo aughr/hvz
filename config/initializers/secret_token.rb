6a00072d405e009bc60ef96ecf5b114a5b13841d
Andrew Bloomgarden <stalkingtiger@gmail.com>
2011-02-05 16:50:09 -0500

    Initial suspenders commit.
    However, RPM replaces hoptoad.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hvz::Application.config.secret_token = '1a6c52a8cec297741831060cfffb90d42527ddd54abe0b3d9b19ef022b1ded065d33a9612263b94efc79fdba61f82082addf5cf82699bed104e0d3194596fd92'
